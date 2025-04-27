68299b628527e52e1dd47d878b2a241db134ef28
Served Smart <195884188+servedsmart@users.noreply.github.com>
2025-04-27 02:55:52 +0200

    Move other needed vars to consent-appearance.js

--- assets/js/appearance.js
+++ assets/js/appearance.js
@@ -39,44 +39,6 @@ if (document.documentElement.getAttribute("data-auto-appearance") === "true") {
     });
 }
 
-var updateMeta = () => {
-  var elem, style;
-  elem = document.querySelector("body");
-  style = getComputedStyle(elem);
-  document
-    .querySelector('meta[name="theme-color"]')
-    .setAttribute("content", style.backgroundColor);
-};
-
-{{ if and (.Site.Params.Logo) (.Site.Params.SecondaryLogo) }}
-{{ $primaryLogo := resources.Get .Site.Params.Logo }}
-{{ $secondaryLogo := resources.Get .Site.Params.SecondaryLogo }}
-{{ if and ($primaryLogo) ($secondaryLogo) }}
-var updateLogo = (targetAppearance) => {
-  var imgElems = document.querySelectorAll("img.logo");
-  var logoContainers = document.querySelectorAll("span.logo");
-
-  targetLogoPath =
-    targetAppearance == "{{ .Site.Params.DefaultAppearance }}"
-      ? "{{ $primaryLogo.RelPermalink }}"
-      : "{{ $secondaryLogo.RelPermalink }}";
-  for (const elem of imgElems) {
-    elem.setAttribute("src", targetLogoPath);
-  }
-
-  {{ if eq $primaryLogo.MediaType.SubType "svg" }}
-  targetContent =
-    targetAppearance == "{{ .Site.Params.DefaultAppearance }}"
-      ? `{{ $primaryLogo.Content | safeHTML }}`
-      : `{{ $secondaryLogo.Content | safeHTML }}`;
-  for (const container of logoContainers) {
-    container.innerHTML = targetContent;
-  }
-  {{ end }}
-};
-{{ end }}
-{{- end }}
-
 window.addEventListener("DOMContentLoaded", (event) => {
   const scroller = document.getElementById("top-scroller");
   const footer = document.getElementById("site-footer");
--- assets/js/consent-appearance.js
+++ assets/js/consent-appearance.js
@@ -8,6 +8,48 @@
  * -----
  */
 
+var getTargetAppearance = () => {
+  return document.documentElement.classList.contains("dark") ? "dark" : "light";
+};
+
+var updateMeta = () => {
+  var elem, style;
+  elem = document.querySelector("body");
+  style = getComputedStyle(elem);
+  document
+    .querySelector('meta[name="theme-color"]')
+    .setAttribute("content", style.backgroundColor);
+};
+
+{{ if and (.Site.Params.Logo) (.Site.Params.SecondaryLogo) }}
+    {{ $primaryLogo := resources.Get .Site.Params.Logo }}
+    {{ $secondaryLogo := resources.Get .Site.Params.SecondaryLogo }}
+{{ if and ($primaryLogo) ($secondaryLogo) }}
+var updateLogo = (targetAppearance) => {
+  var imgElems = document.querySelectorAll("img.logo");
+  var logoContainers = document.querySelectorAll("span.logo");
+
+  targetLogoPath =
+    targetAppearance == "{{ .Site.Params.DefaultAppearance }}"
+      ? "{{ $primaryLogo.RelPermalink }}"
+      : "{{ $secondaryLogo.RelPermalink }}";
+  for (const elem of imgElems) {
+    elem.setAttribute("src", targetLogoPath);
+  }
+
+  {{ if eq $primaryLogo.MediaType.SubType "svg" }}
+  targetContent =
+    targetAppearance == "{{ .Site.Params.DefaultAppearance }}"
+      ? `{{ $primaryLogo.Content | safeHTML }}`
+      : `{{ $secondaryLogo.Content | safeHTML }}`;
+  for (const container of logoContainers) {
+    container.innerHTML = targetContent;
+  }
+  {{ end }}
+};
+{{ end }}
+{{- end }}
+
 window.addEventListener("DOMContentLoaded", (event) => {
   const switcher = document.getElementById("appearance-switcher");
   const switcherMobile = document.getElementById("appearance-switcher-mobile");
@@ -42,7 +84,3 @@ window.addEventListener("DOMContentLoaded", (event) => {
     });
   }
 });
-
-var getTargetAppearance = () => {
-  return document.documentElement.classList.contains("dark") ? "dark" : "light";
-};
